6924499c6530c40b3eb78c3f257ee895a5b10be2
Jerome Jutteau <mojo@couak.net>
2012-12-28 12:12:08 +0100

    Add langage support (french) and automatic langage selection

---
+++ lib/lang/fr.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ *  Jirafeau, your web file repository
+ *  Copyright (C) 2012  Jerome Jutteau <j.jutteau@gmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+$tr = array (
+             'Jirafeau, your web file repository' => 'Jirafeau, votre dépot de fichiers',
+             'Upload a file' => 'Envoyer un fichier',
+             'One time download' => 'Téléchargement unique',
+             'Password' => 'Mot de passe',
+             'Time limit' => 'Limite de temps',
+             'Maximum file size' => 'Taille maximale',
+             'powered by Jirafeau' => 'Propulsé par Jirafeau',
+             'Jirafeau Project' => 'Projet Jirafeau',
+             'None' => 'Aucune',
+             'One minute' => 'Une minute',
+             'One hour' => 'Une heure',
+             'One day' => 'Une journée',
+             'One week' => 'Une semaine',
+             'One month' => 'Un mois',
+             'The file directory is not writable!' => 'Le dossier \'file\' ne peut être écrit.',
+             'The link directory is not writable!' => 'Le dossier \'link\' ne peut être écrit.',
+             'Installer script still present' => 'Le script d\'installation est toujours présent',
+             'Please make sure to delete the installer script "install.php" before continuing.' => 'Assurez vous de supprimer le fichier "install.php" avant de continuer.',
+             'An error occurred.' => 'Une erreur s\'est produite',
+             'File uploaded! Copy the following URL to get it' => 'Fichier envoyé! Copiez le lien suivant pour le télécharger',
+             'This file is valid until the following date' => 'Ce fichier est valable jusqu\'à la date suivante',
+             'Keep the following URL to delete it at any moment' => 'Conservez le lien suivant pour le supprimer à n\'importe quel moment',
+             'Error 404: Not Found' => 'Erreur 404: La page que vous cherchez n\'existe pas ou n\'existe plus',
+             'File not available.' => 'Fichier non disponible',
+             'File has been deleted.' => 'Le fichier a été supprimé.',
+             'The time limit of this file has expired.' => 'La limite de temps est dépassée.',
+             'Password protection' => 'Protection par mot de passe',
+             'Give the password of this file:' => 'Donnez le mot de passe pour ce fichier',
+             'I have the right to download this file' => 'J\'ai le droit de télécharger ce fichier',
+             'Error 403: Forbidden' => 'Erreur 403: Accès interdit',
+            );
+
+ ?>
