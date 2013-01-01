0019ed49158d0b3cbdcd63113f0dc4d640684186
Jerome Jutteau <mojo@couak.net>
2013-01-01 21:50:42 +0100

    footer fix

--- lib/template/footer.php
+++ lib/template/footer.php
@@ -1,5 +1,5 @@
 <div id="copyright">
-<p><a href="https://gitorious.org/jirafeau/"><?php echo t('powered by Jirafeau'); ?></a><br />
+<p><a href="https://gitorious.org/jirafeau/"><?php echo t('powered by Open-Source project Jirafeau'); ?></a><br />
 <a href="http://www.gnu.org/licenses/agpl.html">copyright</a> &copy; 2008-2012 <a href="https://gitorious.org/jirafeau/">
 <?php echo t('Jirafeau Project'); ?></a></p>
 <p><a href="tos.php">Terms of Service</a></p>
