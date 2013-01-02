6c79f21725f4ed25c11a1961194c10e6ef72ab58
Jerome Jutteau <mojo@couak.net>
2013-01-02 23:17:26 +0100

    get header template correction

--- lib/template/header.php
+++ lib/template/header.php
@@ -8,15 +8,27 @@ if (stristr ($_SERVER['HTTP_ACCEPT'], 'application/xhtml+xml'))
 
 header('Content-Type: ' . $content_type);
 
+/* Avoids irritating errors with the installer (no conf file is present then). */
+if (!isset ($cfg['web_root']))
+    $web_root = 'http://' . $_SERVER['HTTP_HOST'] . '/';
+else
+    $web_root = $cfg['web_root'];
+
+if (!isset ($cfg['style']))
+    $style = 'default';
+else
+    $style = $cfg['style'];
+
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
   <title><?php echo t('Jirafeau, your web file repository'); ?></title>
   <meta http-equiv="Content-Type" content="<?php echo $content_type; ?>" />
-  <link href="<?php echo $cfg['web_root'] . 'media/' . $cfg['style'] . '/style.css.php'; ?>" rel="stylesheet" type="text/css" />
+  <link href="<?php echo $web_root . 'media/' . $style . '/style.css.php'; ?>" rel="stylesheet" type="text/css" />
 </head>
 <body>
 
 <div id="content">
-<h1><a href="<?php echo $cfg['web_root']; ?>"><?php echo t('Jirafeau, your web file repository'); ?></a></h1>
+<h1><a href="<?php echo $web_root; ?>"><?php echo t('Jirafeau, your web file repository'); ?></a></h1>
