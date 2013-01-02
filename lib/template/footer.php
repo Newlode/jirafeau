c11a7c2d5c658d67a7a98296d7fcd288443997ed
Jerome Jutteau <mojo@couak.net>
2013-01-02 23:06:24 +0100

    fix TOS link in footer

--- lib/template/footer.php
+++ lib/template/footer.php
@@ -2,7 +2,7 @@
 <p><a href="https://gitorious.org/jirafeau/"><?php echo t('powered by Open-Source project Jirafeau'); ?></a><br />
 <a href="http://www.gnu.org/licenses/agpl.html">copyright</a> &copy; 2008-2012 <a href="https://gitorious.org/jirafeau/">
 <?php echo t('Jirafeau Project'); ?></a></p>
-<p><a href="tos.php">Terms of Service</a></p>
+<p><a href="<?php echo $cfg['web_root'] . '/tos.php'; ?>">Terms of Service</a></p>
 </div>
 </div>
 
