136a8fd4cae166774c4551b8277c91bc8e0a5e24
Jerome Jutteau <mojo@couak.net>
2013-01-02 23:18:35 +0100

    Jirafeau version 1.0

--- lib/settings.php
+++ lib/settings.php
@@ -18,7 +18,7 @@
  */
 /* Jirafeau constants */
 define ('JIRAFEAU_PACKAGE', 'Jirafeau');
-define ('JIRAFEAU_VERSION', '0.9');
+define ('JIRAFEAU_VERSION', '1.0');
 
 /* Directories. */
 define ('VAR_FILES', $cfg['var_root'] . 'files/');
