9fb59d5f9af3618e443aba9cc459f4bc682ccceb
Jerome Jutteau <mojo@couak.net>
2015-01-30 09:23:21 +0100

    fix Jirafeau URL project in footer

--- lib/template/footer.php
+++ lib/template/footer.php
@@ -1,6 +1,6 @@
 
 <div id="copyright">
-<p><a href="https://gitorious.org/jirafeau"></a><br /><a href="http://www.gnu.org/licenses/agpl.html">agplv3</a> | <a href="https://gitorious.org/jirafeau">Jirafeau Project</a> | <a href="<?php echo $cfg['web_root'] . '/tos.php'; ?>">Terms of Service</a></p>
+<p><a href="https://gitlab.com/mojo42/Jirafeau/wikis/home"></a><br /><a href="http://www.gnu.org/licenses/agpl.html">agplv3</a> | <a href="https://gitlab.com/mojo42/Jirafeau/wikis/home">Jirafeau Project</a> | <a href="<?php echo $cfg['web_root'] . '/tos.php'; ?>">Terms of Service</a></p>
 </div>
 </div>
 <div id="jyraphe">
