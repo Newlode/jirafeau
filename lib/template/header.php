75f4119aba4c074acc8f83b82eb3be94ace4aeca
pixelbrackets <mail@pixelbrackets.de>
2017-01-17 22:21:47 +0100

    [!!][FEATURE] Remove XHTML doctype
    The project uses a HTML5 doctype, but switches
    to XHTML if a condition says that the clients browser
    can handle XHTML. As this is the case for nearly any
    browser which also supports HTML5, the condition causes
    the script to *always* render the XHTML doctype.
    
    We remove the whole condition and the XHTML doctype
    in favour of a HTML5 doctype only.
    
    Note: This may be considered as breaking change,
    since HTML5 may not be working in ancient versions of IE.
    
    Refs #97

--- lib/template/header.php
+++ lib/template/header.php
@@ -1,15 +1,6 @@
 <?php
 header('Vary: Accept');
-
-$content_type = 'text/html; charset=utf-8';
-
-if (isset ($_SERVER['HTTP_ACCEPT']) &&
-    stristr ($_SERVER['HTTP_ACCEPT'], 'application/xhtml+xml'))
-{
-    $content_type = 'application/xhtml+xml; charset=utf-8';
-}
-
-header('Content-Type: ' . $content_type);
+header('Content-Type: text/html; charset=utf-8');
 header('x-ua-compatible: ie=edge');
 
 $protocol = (bool)is_ssl() ? 'https' : 'http';
@@ -28,31 +19,16 @@ if (!isset ($cfg['style']))
     $style = 'default';
 else
     $style = $cfg['style'];
-
-if (isset ($_SERVER['HTTP_ACCEPT']) &&
-    stristr ($_SERVER['HTTP_ACCEPT'], 'application/xhtml+xml'))
-{
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">    
-<?php
-}
-else
-{
 ?>
 <!DOCTYPE html>
 <html>
-<?php
-}
-?>
 <head>
+  <meta charset="utf-8">
   <title><?php echo (true === empty($cfg['title']))? t('Jirafeau, your web file repository') : $cfg['title']; ?></title>
-  <meta http-equiv="Content-Type" content="<?php echo $content_type; ?>" />
   <link href="<?php echo $web_root . 'media/' . $style . '/style.css.php'; ?>" rel="stylesheet" type="text/css" />
 </head>
 <body>
-<script type="text/javascript" language="Javascript" src="lib/functions.js.php"></script>
-
+<script type="text/javascript" src="lib/functions.js.php"></script>
 <div id="content">
   <h1>
     <a href="<?php echo $web_root; ?>">
