4a0435033a0b5a97f89ad0e16db8b4ede1265639
Dan Untenzu <untenzu@webit.de>
2017-03-17 15:54:22 +0100

    [FEATURE] Enable custom favicons
    Enable integrators to add custom favicons by
    including it from the media folder (themes).

--- lib/template/header.php
+++ lib/template/header.php
@@ -7,6 +7,7 @@ header('x-ua-compatible: ie=edge');
 <head>
   <meta charset="utf-8">
   <title><?php echo (true === empty($cfg['title']))? t('Jirafeau, your web file repository') : $cfg['title']; ?></title>
+  <link rel="shortcut icon" href="<?php echo JIRAFEAU_ABSPREFIX . 'media/' . $cfg['style'] . '/favicon.ico'; ?>">
   <link href="<?php echo JIRAFEAU_ABSPREFIX . 'media/' . $cfg['style'] . '/style.css.php'; ?>" rel="stylesheet" type="text/css" />
 </head>
 <body>
